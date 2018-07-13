578b6df6f302ad393505739e775187ee6741e70b
Victor Kozlov <punkrus18@gmail.com>
2018-07-13 11:25:32 +0300

    initial commit;

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <div v-if="nos !== null">
+            <app-header :title="title" :nos="nos"></app-header>
+            <router-view :nos="nos"/>
+        </div>
+        <div v-else>
+            Launch me through nOS client, please!
+        </div>
+    </div>
+</template>
+
+<script>
+import AppHeader from './components/app-header'
+const nosApi = window.NOS ? window.NOS.V1 : null
+
+export default {
+  name: 'app',
+  components: { AppHeader },
+
+  data: function () {
+    return {
+      nos: nosApi,
+      title: 'nOS dapp'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
